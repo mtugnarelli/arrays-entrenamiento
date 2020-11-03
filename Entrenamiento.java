480557bb9399639316662c0f5c4de35ec754d35e
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2020-11-03 01:46:31 -0300

    Atomicos borrados

--- Entrenamiento.java
+++ Entrenamiento.java
@@ -3,13 +3,6 @@ public class Entrenamiento {
 
 	private double[] recorridos = new double[5];
 	
-	private double recorrido1;
-	private double recorrido2;
-	private double recorrido3;
-	private double recorrido4;
-	private double recorrido5;
-	
-
 	/**
 	 * pre : 'dia' está en rango [1-5] y 'distancia' es mayor o igual a 0.
 	 * post: registra la distancia recorrida en el día indicado. 
