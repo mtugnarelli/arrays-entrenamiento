524e7899222169dd177b10a50fec2bc4af92be8c
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2020-11-03 00:42:16 -0300

    calcularRecorridoPromedio

--- Entrenamiento.java
+++ Entrenamiento.java
@@ -6,4 +6,12 @@ public class Entrenamiento {
 	double recorrido3;
 	double recorrido4;
 	double recorrido5;
+	
+	double calcularRecorridoPromedio() {
+		
+		return (recorrido1 + recorrido2 + 
+				recorrido3 + recorrido4 + 
+				recorrido5) / 5;
+				
+	}
 }
