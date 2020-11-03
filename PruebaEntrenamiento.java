3030fdad7ba3f0e3bfd4443cba0ffc2aae9059bf
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2020-11-03 00:49:54 -0300

    Prueba promedio implementada

--- PruebaEntrenamiento.java
+++ PruebaEntrenamiento.java
@@ -7,6 +7,15 @@ public class PruebaEntrenamiento {
 	@Test
 	public void calcularRecorridoPromedioConEntrenamientoRegularDe10Km() {
 		
+		Entrenamiento entrenamiento = new Entrenamiento();
+		entrenamiento.recorrido1 = 10;
+		entrenamiento.recorrido2 = 10;
+		entrenamiento.recorrido3 = 10;
+		entrenamiento.recorrido4 = 10;
+		entrenamiento.recorrido5 = 10;
 		
+		double promedio = entrenamiento.calcularRecorridoPromedio();
+		
+		Assert.assertEquals(10, promedio, 0.1);
 	}
 }
